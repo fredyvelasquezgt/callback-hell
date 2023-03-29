f4d780ac5a4c7713a8fa67ed3c7f0669f7ecd59c
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-29 12:44:03 -0600

    finished promises

--- promises.js
+++ promises.js
@@ -12,3 +12,20 @@ const readMovieFilePromise = (fileName) => {
 }
 
 readMovieFilePromise('movie.txt')
+    .then(query => {
+        return 'https://api.themoviedb.org/3/search/movie?api_key=12724b00b4c063fbc5f09f5729c5aec8&query='+query.toString()
+    })
+    .then(url => {
+        request(url, {timeout: 0}, (error, response, body) => {
+
+        let movies = JSON.parse(body);
+
+        movies.results.forEach((movie) => {
+            console.log(movie.original_title)
+        })
+    })
+
+})
+.catch(error => {
+    console.log(error)
+})
