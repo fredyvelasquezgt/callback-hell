9319844e1befa6821169c2a8d01ed4516196e8c1
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-30 12:16:31 -0600

    new method finished

--- asyncawait.js
+++ asyncawait.js
@@ -14,7 +14,23 @@ const readMovieFilePromise = (fileName) => {
 //esta funcion es async para que nuestro codigo no se bloquee
 //el await detiene todos los procesos y espera hasta que la promesa sea ejecutada
 const getAllMovies = async() => {
-    let query = await readMovieFilePromise('movie.txt');
-    
 
+    try {
+        let query = await readMovieFilePromise('movie.txt');
+    let url = 'https://api.themoviedb.org/3/search/movie?api_key=12724b00b4c063fbc5f09f5729c5aec8&query='+query.toString();
+
+    await request(url,{timeout:0})
+        .then(body => {
+        let movies = JSON.parse(body)
+
+        movies.results.forEach((movie) => {
+            console.log(movie.original_title)
+        })
+    })
+
+    } catch(error) {
+        console.log(error)
+    } 
 }
+
+getAllMovies();
